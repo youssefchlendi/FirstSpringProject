3c3368aedbadeb4ff15d6c68672685815087e676
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-04 14:17:02 +0100

    Create Produit class , att and get/setters

---
+++ src/main/java/com/iset/produits/entities/Produit.java
@@ -0,0 +1,48 @@
+package com.iset.produits.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produit {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long idProduit;
+	private String nomProduit;
+	private Double prixProduit;
+	private Date dateCreation;
+	public String getNomProduit() {
+		return nomProduit;
+	}
+	public void setNomProduit(String nomProduit) {
+		this.nomProduit = nomProduit;
+	}
+	public Double getPrixProduit() {
+		return prixProduit;
+	}
+	public void setPrixProduit(Double prixProduit) {
+		this.prixProduit = prixProduit;
+	}
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+	public Produit(String nomProduit, Double prixProduit, Date dateCreation) {
+		super();
+		this.nomProduit = nomProduit;
+		this.prixProduit = prixProduit;
+		this.dateCreation = dateCreation;
+	}
+	public Produit() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
