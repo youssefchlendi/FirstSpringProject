0cca06e3d712829e82e623b5141cf0ab18da2dd6
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-06 00:27:31 +0100

    unit test to test Produit Service

--- src/test/java/com/iset/produits/ProduitsApplicationTests.java
+++ src/test/java/com/iset/produits/ProduitsApplicationTests.java
@@ -9,13 +9,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.iset.produits.dao.ProduitRepository;
 import com.iset.produits.entities.Produit;
+import com.iset.produits.service.ProduitServiceImpl;
 
 
 @SpringBootTest
 class ProduitsApplicationTests {
 
 	@Autowired
-	private ProduitRepository produitRepository;
+	private ProduitServiceImpl service;
+//	private ProduitRepository produitRepository;
 	
 	@Test
 	void contextLoads() {
@@ -24,36 +26,41 @@ class ProduitsApplicationTests {
 	@Test
 	public void testCreateProduit() {
 	Produit prod = new Produit("PC Asus",1500.500,new Date());
-	produitRepository.save(prod);
+	service.saveProduit(prod);
+//	produitRepository.save(prod);
 	}
-	
 	@Test
-	public void testFindProduit()
+	public void testFindProduit	()
 	{
-	Produit p = produitRepository.findById(1L).get();
+	Produit p = service.getProduit(1L); 
+//			produitRepository.findById(1L).get();
 	System.out.println(p);
 	}
-	
+//	
 	@Test
 	public void testUpdateProduit()
 	{
-	Produit p = produitRepository.findById(1L).get();
+	Produit p = service.getProduit(1L); 
+//			produitRepository.findById(1L).get();
 	p.setPrixProduit(2000.0);
 	p.setNomProduit("PC acer");
-	produitRepository.save(p);
+	service.saveProduit(p);
+//	produitRepository.save(p);
 	System.out.println(p);
 	}
-	
+//	
 	@Test
 	public void testDeleteProduit()
 	{
-	produitRepository.deleteById(1L);
+	service.deleteProduitById(1L);
+//	produitRepository.deleteById(1L);
 	}
-	
+//	
 	@Test
 	public void testFindAllProduits()
 	{
-	List<Produit> prods = produitRepository.findAll();
+	List<Produit> prods = service.getAllProduits();
+//			produitRepository.findAll();
 	for (Produit p:prods)
 	System.out.println(p.getNomProduit());
 	}
