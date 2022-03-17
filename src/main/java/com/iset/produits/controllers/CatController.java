184716e03077d52d85c897894966dd79f6bf170a
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-18 00:07:27 +0100

    add pagination to all methods

--- src/main/java/com/iset/produits/controllers/CatController.java
+++ src/main/java/com/iset/produits/controllers/CatController.java
@@ -11,9 +11,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.servlet.view.RedirectView;
 
 import com.iset.produits.entities.Produit;
 import com.iset.produits.service.ProduitService;
+import org.springframework.data.domain.Page;
 
 @Controller
 public class CatController {
@@ -27,43 +29,51 @@ public class CatController {
 
 	@RequestMapping("/saveProduit")
 	public String saveProduit(@ModelAttribute("produit") Produit produit, @RequestParam("date") String date,
-			ModelMap modelMap) throws ParseException {
-		// conversion de la date
+			ModelMap modelMap,
+			@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "2") int size) throws ParseException {
 		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
 		Date dateCreation = dateformat.parse(String.valueOf(date));
 		produit.setDateCreation(dateCreation);
 		Produit saveProduit = produitService.saveProduit(produit);
 		String msg = "produit enregistré avec Id " + saveProduit.getIdProduit();
-		List<Produit> prods = produitService.getAllProduits();
+		Page<Produit> prods = produitService.getAllProduitsParPage(page, size);
 		modelMap.addAttribute("produits", prods);
+		modelMap.addAttribute("pages", new int[prods.getTotalPages()]);
+
 		modelMap.addAttribute("msg", msg);
 		modelMap.addAttribute("type", "success");
 		return "listeProduits";
 	}
 
 	@RequestMapping("/ListeProduits")
-	public String listeProduits(ModelMap modelMap) {
-		List<Produit> prods = produitService.getAllProduits();
+	public String listeProduits(ModelMap modelMap, @RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "2") int size) {
+		Page<Produit> prods = produitService.getAllProduitsParPage(page, size);
 		modelMap.addAttribute("produits", prods);
+		modelMap.addAttribute("pages", new int[prods.getTotalPages()]);
+		modelMap.addAttribute("currentPage", page);
 		return "listeProduits";
 	}
 
 	@RequestMapping("/supprimerProduit")
-	public String supprimerProduit(@RequestParam("id") Long id, ModelMap modelMap) {
+	public String supprimerProduit(@RequestParam("id") Long id, ModelMap modelMap,
+			@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "2") int size) {
 		Produit p = new Produit();
 		Produit p1 = produitService.getProduit(id);
 		p.setIdProduit(id);
 		produitService.deleteProduit(p);
-		List<Produit> prods = produitService.getAllProduits();
+		Page<Produit> prods = produitService.getAllProduitsParPage(page, size);
 		modelMap.addAttribute("produits", prods);
-		if (p1!=null){
+		modelMap.addAttribute("pages", new int[prods.getTotalPages()]);
+		if (p1 != null) {
 			modelMap.addAttribute("msg", "Produit supprimée");
 			modelMap.addAttribute("type", "danger");
-		}else{
+		} else {
 			modelMap.addAttribute("msg", "Id non existant");
 			modelMap.addAttribute("type", "warning");
 		}
-
 		return "listeProduits";
 	}
 
@@ -76,14 +86,17 @@ public class CatController {
 
 	@RequestMapping("/updateProduit")
 	public String updateProduit(@ModelAttribute("produit") Produit produit, @RequestParam("date") String date,
-			ModelMap modelMap) throws ParseException {
+			ModelMap modelMap,
+			@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "2") int size) throws ParseException {
 		// conversion de la date
 		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
 		Date dateCreation = dateformat.parse(String.valueOf(date));
 		produit.setDateCreation(dateCreation);
-		produitService.updateProduit(produit.getIdProduit(),produit);
-		List<Produit> prods = produitService.getAllProduits();
+		produitService.updateProduit(produit.getIdProduit(), produit);
+		Page<Produit> prods = produitService.getAllProduitsParPage(page, size);
 		modelMap.addAttribute("produits", prods);
+		modelMap.addAttribute("pages", new int[prods.getTotalPages()]);
 		modelMap.addAttribute("msg", "Produit modifiée");
 		modelMap.addAttribute("type", "warning");
 
