15bf69d7d6372a38091b54c396bc08ff77aa16fe
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-30 10:53:03 +0100

    add filtering methods

--- src/main/java/com/iset/produits/dao/ProduitRepository.java
+++ src/main/java/com/iset/produits/dao/ProduitRepository.java
@@ -1,8 +1,30 @@
 package com.iset.produits.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
+import com.iset.produits.entities.Categorie;
 import com.iset.produits.entities.Produit;
-public interface ProduitRepository extends JpaRepository<Produit, Long> {
 
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+    List<Produit> findByNomProduit(String nom);
+
+    List<Produit> findByNomProduitContains(String nom);
+
+    @Query("select p from Produit p where p.nomProduit like %?1 and p.prixProduit> ?2")
+    List<Produit> findByNomPrix(String nom, Double prix);
+
+    @Query("select p from Produit p where p.categorie = ?1")
+    List<Produit> findByCategorie(Categorie categorie);
+
+    List<Produit> findByCategorieIdCat(Long id);
+
+    List<Produit> findByOrderByNomProduitAsc();
+
+    @Query("select p from Produit p order by p.nomProduit ASC, p.prixProduit DESC")
+    List<Produit> trierProduitsNomsPrix();
 }
