80eb10d534bf185d51cf4672c9801ceb813e29e9
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-30 10:54:59 +0100

    add categorie relation to produit

--- src/main/java/com/iset/produits/entities/Produit.java
+++ src/main/java/com/iset/produits/entities/Produit.java
@@ -6,39 +6,50 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Produit {
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idProduit;
 	private String nomProduit;
 	private Double prixProduit;
 	private Date dateCreation;
+	@ManyToOne
+	private Categorie categorie;
+
 	public String getNomProduit() {
 		return nomProduit;
 	}
+
 	public void setNomProduit(String nomProduit) {
 		this.nomProduit = nomProduit;
 	}
+
 	public Double getPrixProduit() {
 		return prixProduit;
 	}
+
 	public void setPrixProduit(Double prixProduit) {
 		this.prixProduit = prixProduit;
 	}
+
 	public Date getDateCreation() {
 		return dateCreation;
 	}
+
 	public void setDateCreation(Date dateCreation) {
 		this.dateCreation = dateCreation;
 	}
+
 	public Produit(String nomProduit, Double prixProduit, Date dateCreation) {
 		super();
 		this.nomProduit = nomProduit;
 		this.prixProduit = prixProduit;
 		this.dateCreation = dateCreation;
 	}
+
 	public Produit() {
 		super();
 	}
@@ -46,22 +57,19 @@ public class Produit {
 	public void setIdProduit(Long idProduit) {
 		this.idProduit = idProduit;
 	}
+
 	public Long getIdProduit() {
 		return this.idProduit;
 	}
 
-
 	@Override
 	public String toString() {
 		return "{" +
-			" idProduit='" + getIdProduit() + "'" +
-			", nomProduit='" + getNomProduit() + "'" +
-			", prixProduit='" + getPrixProduit() + "'" +
-			", dateCreation='" + getDateCreation() + "'" +
-			"}";
+				" idProduit='" + getIdProduit() + "'" +
+				", nomProduit='" + getNomProduit() + "'" +
+				", prixProduit='" + getPrixProduit() + "'" +
+				", dateCreation='" + getDateCreation() + "'" +
+				"}";
 	}
-	
 
-
-	
 }
