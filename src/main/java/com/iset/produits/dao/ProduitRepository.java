f9dac4e0f3810c8c67b2b4dc1d4037ada1d3ca19
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-30 10:54:27 +0100

    add filter by name and paginate method

--- src/main/java/com/iset/produits/dao/ProduitRepository.java
+++ src/main/java/com/iset/produits/dao/ProduitRepository.java
@@ -14,6 +14,7 @@ public interface ProduitRepository extends JpaRepository<Produit, Long> {
     List<Produit> findByNomProduit(String nom);
 
     List<Produit> findByNomProduitContains(String nom);
+    Page<Produit> findAllByNomProduitContains(String nom,Pageable pageable);
 
     @Query("select p from Produit p where p.nomProduit like %?1 and p.prixProduit> ?2")
     List<Produit> findByNomPrix(String nom, Double prix);
