a7d9d444d36a1e2c30ea94ea8633dc37d6749c49
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-07 14:12:55 +0100

    add Validation

--- src/main/java/com/iset/produits/entities/Produit.java
+++ src/main/java/com/iset/produits/entities/Produit.java
@@ -7,15 +7,34 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Produit {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idProduit;
+	@NotNull
+	@Size(min = 4, max = 15)
 	private String nomProduit;
+	@NotNull
+	@Min(value = 10)
+	@Max(value = 10000)
 	private Double prixProduit;
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	@PastOrPresent
 	private Date dateCreation;
+	
 	@ManyToOne
 	private Categorie categorie;
 
@@ -64,12 +83,8 @@ public class Produit {
 
 	@Override
 	public String toString() {
-		return "{" +
-				" idProduit='" + getIdProduit() + "'" +
-				", nomProduit='" + getNomProduit() + "'" +
-				", prixProduit='" + getPrixProduit() + "'" +
-				", dateCreation='" + getDateCreation() + "'" +
-				"}";
+		return "{" + " idProduit='" + getIdProduit() + "'" + ", nomProduit='" + getNomProduit() + "'"
+				+ ", prixProduit='" + getPrixProduit() + "'" + ", dateCreation='" + getDateCreation() + "'" + "}";
 	}
 
 }
