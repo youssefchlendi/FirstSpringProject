5ce2cd8a56e281cb959680d046dc5184f4b4cfec
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-13 09:36:35 +0100

    add routes SecurityController

--- src/main/java/com/iset/produits/security/SecurityController.java
+++ src/main/java/com/iset/produits/security/SecurityController.java
@@ -5,5 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class SecurityController {
-    
+    @GetMapping("/accessDenied")
+    public String geterror() {
+        return "accessDenied";
+    }
+
+    @PostMapping("/accessDenied")
+    public String posterror() {
+        return "accessDenied";
+    }
 }
