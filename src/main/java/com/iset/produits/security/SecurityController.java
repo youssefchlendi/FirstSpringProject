df70bb08e3ddd900e526d06d29bb8fc90a76fa18
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-13 09:36:18 +0100

    create securityController

---
+++ src/main/java/com/iset/produits/security/SecurityController.java
@@ -0,0 +1,9 @@
+package com.iset.produits.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+@Controller
+public class SecurityController {
+    
+}
