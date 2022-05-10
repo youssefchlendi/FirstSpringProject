bee57cfd59e1d7ba078363ef6671bee212568296
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-11 00:11:59 +0100

    add findbyid and findall roles

--- src/main/java/com/iset/produits/dao/RoleRepository.java
+++ src/main/java/com/iset/produits/dao/RoleRepository.java
@@ -1,9 +1,14 @@
 package com.iset.produits.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.iset.produits.entities.Role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
     Role findByName(String role);
+    Optional<Role> findById(Long id);
+    List<Role> findAll();
 }
