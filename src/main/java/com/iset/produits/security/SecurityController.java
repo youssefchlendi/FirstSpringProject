8b6d07dc36def538f3d855c5bea5c287e349d101
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-11 00:13:06 +0100

    show logout message on logout

--- src/main/java/com/iset/produits/security/SecurityController.java
+++ src/main/java/com/iset/produits/security/SecurityController.java
@@ -3,6 +3,13 @@ package com.iset.produits.security;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+import com.iset.produits.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SecurityController {
+    @Autowired
+    public UserService userDetailsService;
+
     @GetMapping("/accessDenied")
     public String geterror() {
         return "accessDenied";
@@ -22,9 +32,15 @@ public class SecurityController {
     }
 
     @GetMapping("/login")
-    public String login(ModelMap modelMap,@RequestParam(name = "error", defaultValue = "0") int error) {
+    public String login(ModelMap modelMap, 
+    @RequestParam(name = "error", defaultValue = "0") int error,
+    @RequestParam(name = "logout", defaultValue = "0") int logout) {
         if (error == 1) {
             modelMap.addAttribute("error", "Veuillez vérifier vos identifiants");
+            return "auth";
+        }
+        if (logout == 1) {
+            modelMap.addAttribute("error", "Vous avez été déconnecté");
             return "auth";
         }
         return "auth";
