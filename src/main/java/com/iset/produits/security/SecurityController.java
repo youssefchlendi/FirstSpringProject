b5919f8c7933f654d76cc9f8ff3fbe93819836d5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-10 17:02:22 +0100

    add login logout loggedout routes

--- src/main/java/com/iset/produits/security/SecurityController.java
+++ src/main/java/com/iset/produits/security/SecurityController.java
@@ -1,8 +1,14 @@
 package com.iset.produits.security;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class SecurityController {
     @GetMapping("/accessDenied")
@@ -13,5 +19,26 @@ public class SecurityController {
     @PostMapping("/accessDenied")
     public String posterror() {
         return "accessDenied";
+    }
+
+    @GetMapping("/login")
+    public String login(ModelMap modelMap,@RequestParam(name = "error", defaultValue = "0") int error) {
+        if (error == 1) {
+            modelMap.addAttribute("error", "Veuillez vérifier vos identifiants");
+            return "login";
+        }
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        // redirect to login page
+        return "redirect:/login";
+    }
+
+    @GetMapping("/loggedout")
+    public String loggedout() {
+        return "loggedout";
     }
 }
