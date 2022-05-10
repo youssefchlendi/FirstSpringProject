b21008c3302aafa0f7a4795efc6a4193ffecccc1
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-10 19:36:01 +0100

    add findbyname method to role repo

--- src/main/java/com/iset/produits/dao/RoleRepository.java
+++ src/main/java/com/iset/produits/dao/RoleRepository.java
@@ -5,5 +5,5 @@ import com.iset.produits.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
-    
+    Role findByName(String role);
 }
