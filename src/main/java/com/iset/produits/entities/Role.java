a7363d4ac3966a3fecd31ffbb93e64d638e591a3
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-10 17:00:17 +0100

    create role entity

---
+++ src/main/java/com/iset/produits/entities/Role.java
@@ -0,0 +1,47 @@
+package com.iset.produits.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+
+    public Role() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Role(String name) {
+        super();
+        this.name = name;
+    }
+}
