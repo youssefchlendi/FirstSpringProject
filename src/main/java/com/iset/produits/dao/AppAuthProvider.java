9385214503c1e656e2a558ddba0e827952573897
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-10 17:02:36 +0100

    create AppAuthProvider class

---
+++ src/main/java/com/iset/produits/dao/AppAuthProvider.java
@@ -0,0 +1,35 @@
+package com.iset.produits.dao;
+
+
+import com.iset.produits.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AppAuthProvider extends DaoAuthenticationProvider {
+    @Autowired
+    UserService userDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) authentication;
+        String name = auth.getName();
+        String password = auth.getCredentials().toString();
+        UserDetails user = userDetailsService.loadUserByUsername(name);
+        if (user == null) {
+            throw new BadCredentialsException("Username/Password does not match for "
+                    + auth.getPrincipal());
+        }
+        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
