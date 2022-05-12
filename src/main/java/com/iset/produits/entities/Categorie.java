9fc47a9b8f38d1e3dbd2a3269165eb9c611c4828
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-12 09:28:32 +0100

    create category entity

--- src/main/java/com/iset/produits/entities/Categorie.java
+++ src/main/java/com/iset/produits/entities/Categorie.java
@@ -6,29 +6,38 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Categorie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCat;
+    @NotNull
+	@Size(min = 4, max = 255)
     private String nomCat;
+    @NotNull
+	@Size(min = 4, max = 255)
     private String descriptionCat;
     @OneToMany(mappedBy = "categorie")
     private List<Produit> produits;
 
+
     public Categorie() {
     }
 
-    public Categorie(String nomCat, String descriptionCat, List<Produit> produits) {
-        super();
+
+    public Categorie(Long idCat, String nomCat, String descriptionCat, List<Produit> produits) {
+        this.idCat = idCat;
         this.nomCat = nomCat;
         this.descriptionCat = descriptionCat;
         this.produits = produits;
     }
 
+
     public Long getIdCat() {
-        return idCat;
+        return this.idCat;
     }
 
     public void setIdCat(Long idCat) {
@@ -36,7 +45,7 @@ public class Categorie {
     }
 
     public String getNomCat() {
-        return nomCat;
+        return this.nomCat;
     }
 
     public void setNomCat(String nomCat) {
@@ -44,7 +53,7 @@ public class Categorie {
     }
 
     public String getDescriptionCat() {
-        return descriptionCat;
+        return this.descriptionCat;
     }
 
     public void setDescriptionCat(String descriptionCat) {
@@ -52,11 +61,12 @@ public class Categorie {
     }
 
     public List<Produit> getProduits() {
-        return produits;
+        return this.produits;
     }
 
     public void setProduits(List<Produit> produits) {
         this.produits = produits;
     }
+    
 
 }
